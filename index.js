fd3cc84d35a1893f66bfa08254f198fc86f832b7
Tanish Jain <tanishjain190702@gmail.com>
2022-03-23 20:13:53 +0530

    New repo

---
+++ index.js
@@ -0,0 +1,45 @@
+let saveEl=document.getElementById("save-el")
+let ansEl=document.getElementById("ans-el")
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+
+let count=0
+let countStr
+console.log(saveEl)
+console.log(ansEl)
+
+function increment(){
+    
+    count=count+1
+    countEl.innerText= count
+}
+
+function decrement(){
+    
+    if(count==0){
+    countEl.innerText= 0}
+    else{
+        count-=1 
+    countEl.innerText= count
+    }
+}
+
+function save(){
+    let countStr = count + "+"
+    saveEl.innerText+= countStr 
+    
+}
+
+function reset(){
+    count=0
+    countEl.innerText= count
+    
+}
+
+function reset2(){
+
+    save=0
+    saveEl.innerText= save
+    
+    
+}
